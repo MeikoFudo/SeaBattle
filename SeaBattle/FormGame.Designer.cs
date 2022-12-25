2aca4a957f50aace7cd87b6411daf7adb39c463f
MeikoFudo <akokoeva000@gmail.com>
2022-12-25 20:02:09 +0300

    Добавлен массив для хранения кнопок, метка при попадании и метка при промахе

--- SeaBattle/FormGame.Designer.cs
+++ SeaBattle/FormGame.Designer.cs
@@ -30,12 +30,12 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // FormGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(914, 564);
+            this.Name = "FormGame";
             this.Text = "FormGame";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
--- SeaBattle/FormGame.cs
+++ SeaBattle/FormGame.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,18 +14,23 @@ namespace SeaBattle
     public partial class FormGame : Form
     {
         public const int SIZEMAP = 10;
-        public int SIZECELL = 90;
+        public int SIZECELL = 40;
         public string MARKUP = "ABCDEFGHIJK";
+
         public int[,] mapPlayer = new int[SIZEMAP, SIZEMAP];
         public int[,] opponentMap = new int[SIZEMAP, SIZEMAP];
+
+        public bool isPlaying = false;
         public FormGame()
         {
             InitializeComponent();
+            this.Text = "SeaBattle";
             Init();
         }
 
         public void Init()
         {
+            isPlaying = false;
             CreateMaps();
         }
         private void Form1_Load(object sender, EventArgs e)
@@ -40,13 +46,28 @@ namespace SeaBattle
                 for (int j = 0; j < SIZEMAP; j++)
                 {
                     mapPlayer[i, j] = 0;
+
                     Button button = new Button();
                     button.Size = new Size(SIZECELL, SIZECELL);
+                    button.BackColor = Color.White;
                     button.Location = new Point(j * SIZECELL, i * SIZECELL);
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                            button.Text = MARKUP[j - 1].ToString();
+                        if (j == 0 && i > 0)
+                            button.Text = i.ToString();
+                    }
+                    else
+                    {
+                        button.Click += new EventHandler(ConfShips);
+                    }
                     this.Controls.Add(button);
+
                 }
             }
-            
+
 
             for (int i = 0; i < SIZEMAP; i++)
             {
@@ -54,12 +75,84 @@ namespace SeaBattle
                 {
                     opponentMap[i, j] = 0;
                     mapPlayer[i, j] = 0;
+
                     Button button = new Button();
                     button.Size = new Size(SIZECELL, SIZECELL);
-                    button.Location = new Point(350+j * SIZECELL, i * SIZECELL);
+                    button.BackColor = Color.White;
+                    button.Location = new Point(450 + j * SIZECELL, i * SIZECELL);
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                            button.Text = MARKUP[j - 1].ToString();
+                        if (j == 0 && i > 0)
+                            button.Text = i.ToString();
+                    }
                     this.Controls.Add(button);
                 }
             }
+            Label map1 = new Label();
+            map1.Text = "Player 1";
+            map1.Location = new Point(SIZEMAP * SIZECELL / 2, SIZEMAP * SIZECELL + 10);
+            this.Controls.Add(map1);
+
+            Label map2 = new Label();
+            map2.Text = "Player 2";
+            map2.Location = new Point(450 + SIZEMAP * SIZECELL / 2, SIZEMAP * SIZECELL + 10);
+            this.Controls.Add(map2);
+
+            Button start = new Button();
+            start.Text = "START";
+            start.Click += new EventHandler(StartPlay);
+            start.Location = new Point(0, SIZEMAP * SIZECELL + 20);
+            this.Controls.Add(start);
+        }
+        public void StartPlay(object sender, EventArgs e)
+        {
+            isPlaying = true;
+        }
+
+        public void ConfShips(object sender, EventArgs e)
+        {
+            Button press = sender as Button;
+            if (isPlaying)
+            {
+                if (mapPlayer[press.Location.Y / SIZECELL, press.Location.X / SIZECELL] == 0)
+                {
+                    press.BackColor = Color.Red;
+                    mapPlayer[press.Location.Y / SIZECELL, press.Location.X / SIZECELL] = 1;
+                }
+                else
+                {
+                    press.BackColor = Color.White;
+                    mapPlayer[press.Location.Y / SIZECELL, press.Location.X / SIZECELL] = 0;
+                }
+            }
+        }
+
+        public bool Shoot(int[,] map, Button press)
+        {
+            bool hit = false;
+            if (isPlaying)
+            {
+                int delta = 0;
+                if (press.Location.X > 320)
+                    delta = 320;
+                if (map[press.Location.Y / SIZECELL, (press.Location.X - delta) / SIZECELL] != 0)
+                {
+                    hit = true;
+                    map[press.Location.Y / SIZECELL, (press.Location.X - delta) / SIZECELL] = 0;
+                    press.BackColor = Color.Green;
+                    press.Text = "X";
+                }
+                else
+                {
+                    hit = false;
+
+                    press.BackColor = Color.Black;
+                }
+            }
+            return hit;
         }
     }
 }
