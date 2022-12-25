6a4574873b8b65c7ddd32cee908c8377a7f7f1ee
MeikoFudo <akokoeva000@gmail.com>
2022-12-25 21:51:26 +0300

    добавлен класс оппонент

--- SeaBattle/FormGame.cs
+++ SeaBattle/FormGame.cs
@@ -14,13 +14,16 @@ namespace SeaBattle
     public partial class FormGame : Form
     {
         public const int SIZEMAP = 10;
-        public int SIZECELL = 40;
+        public int SIZECELL = 30;
         public string MARKUP = "ABCDEFGHIJK";
 
         public int[,] mapPlayer = new int[SIZEMAP, SIZEMAP];
         public int[,] opponentMap = new int[SIZEMAP, SIZEMAP];
 
         public bool isPlaying = false;
+
+        public Button[,] allMyButtons = new Button[SIZEMAP, SIZEMAP];
+        public Button[,] allOpponentButtons = new Button[SIZEMAP, SIZEMAP];
         public FormGame()
         {
             InitializeComponent();
@@ -28,10 +31,13 @@ namespace SeaBattle
             Init();
         }
 
+        public Opponent opponent;
         public void Init()
         {
             isPlaying = false;
             CreateMaps();
+            opponent = new Opponent(opponentMap, mapPlayer, allOpponentButtons, allMyButtons);
+            opponentMap = opponent.Ships();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -39,8 +45,8 @@ namespace SeaBattle
         }
         public void CreateMaps()
         {
-            this.Width = SIZEMAP * SIZECELL * 2 + 70;
-            this.Height = SIZEMAP * SIZECELL;
+            this.Width = SIZEMAP * SIZECELL * 2 + 50;
+            this.Height = (SIZEMAP + 3) * SIZECELL + 20;
             for (int i = 0; i < SIZEMAP; i++)
             {
                 for (int j = 0; j < SIZEMAP; j++)
@@ -63,6 +69,7 @@ namespace SeaBattle
                     {
                         button.Click += new EventHandler(ConfShips);
                     }
+                    allMyButtons[i, j] = button;
                     this.Controls.Add(button);
 
                 }
@@ -88,6 +95,11 @@ namespace SeaBattle
                         if (j == 0 && i > 0)
                             button.Text = i.ToString();
                     }
+                    else
+                    {
+                        button.Click += new EventHandler(PlayerShoot);
+                    }
+                    allOpponentButtons[i, j] = button;
                     this.Controls.Add(button);
                 }
             }
@@ -98,7 +110,7 @@ namespace SeaBattle
 
             Label map2 = new Label();
             map2.Text = "Player 2";
-            map2.Location = new Point(450 + SIZEMAP * SIZECELL / 2, SIZEMAP * SIZECELL + 10);
+            map2.Location = new Point(350 + SIZEMAP * SIZECELL / 2, SIZEMAP * SIZECELL + 10);
             this.Controls.Add(map2);
 
             Button start = new Button();
@@ -135,13 +147,13 @@ namespace SeaBattle
             bool hit = false;
             if (isPlaying)
             {
-                int delta = 0;
+                int offset = 0;
                 if (press.Location.X > 320)
-                    delta = 320;
-                if (map[press.Location.Y / SIZECELL, (press.Location.X - delta) / SIZECELL] != 0)
+                    offset = 320;
+                if (map[press.Location.Y / SIZECELL, (press.Location.X - offset) / SIZECELL] != 0)
                 {
                     hit = true;
-                    map[press.Location.Y / SIZECELL, (press.Location.X - delta) / SIZECELL] = 0;
+                    map[press.Location.Y / SIZECELL, (press.Location.X - offset) / SIZECELL] = 0;
                     press.BackColor = Color.Green;
                     press.Text = "X";
                 }
@@ -153,6 +165,39 @@ namespace SeaBattle
                 }
             }
             return hit;
+        }
+
+        public bool CheckIfMapIsNotEmpty()
+        {
+            bool isEmpty1 = true;
+            bool isEmpty2 = true;
+            for (int i = 1; i < SIZEMAP; i++)
+            {
+                for (int j = 1; j < SIZEMAP; j++)
+                {
+                    if (mapPlayer[i, j] != 0)
+                        isEmpty1 = false;
+                    if (opponentMap[i, j] != 0)
+                        isEmpty2 = false;
+                }
+            }
+            if (isEmpty1 || isEmpty2)
+                return false;
+            else return true;
+        }
+        public void PlayerShoot(object sender, EventArgs e)
+        {
+
+            Button pressedButton = sender as Button;
+            bool playerTurn = Shoot(opponentMap, pressedButton);
+            if (!playerTurn)
+                opponent.Shoot();
+
+            if (!CheckIfMapIsNotEmpty())
+            {
+                this.Controls.Clear();
+                Init();
+            }
         }
     }
 }
---
+++ SeaBattle/Opponent.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeaBattle
+{
+    public class Opponent
+    {
+        public int[,] mapPlayer = new int[FormGame.SIZEMAP, FormGame.SIZEMAP];
+        public int[,] opponentMap = new int[FormGame.SIZEMAP, FormGame.SIZEMAP];
+
+        public Button[,] allMyButtons = new Button[FormGame.SIZEMAP, FormGame.SIZEMAP];
+        public Button[,] allOpponentButtons = new Button[FormGame.SIZEMAP, FormGame.SIZEMAP];
+
+        public Opponent(int[,] mapPlayer, int[,] opponentMap, Button[,] allMyButtons, Button[,] allOpponentButtons)
+        {
+            this.mapPlayer = mapPlayer;
+            this.opponentMap = opponentMap;
+            this.allOpponentButtons = allOpponentButtons;
+            this.allMyButtons = allMyButtons;
+        }
+
+        public bool Border(int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= FormGame.SIZEMAP || j >= FormGame.SIZEMAP)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsEmpty(int i, int j, int length)
+        {
+            bool isEmpty = true;
+
+            for (int k = j; k < j + length; k++)
+            {
+                if (mapPlayer[i, k] != 0)
+                {
+                    isEmpty = false;
+                    break;
+                }
+            }
+
+            return isEmpty;
+        }
+
+        public int[,] Ships()
+        {
+            int SHIPLENGTH = 4;
+            int DELTA = 4;
+            int COUNTSHIPS = 10;
+            Random r = new Random();
+
+            int X = 0;
+            int Y = 0;
+
+            while (COUNTSHIPS > 0)
+            {
+                for (int i = 0; i < DELTA / 4; i++)
+                {
+                    X = r.Next(1, FormGame.SIZEMAP);
+                    Y = r.Next(1, FormGame.SIZEMAP);
+
+                    while (!Border(X, Y + SHIPLENGTH - 1) || !IsEmpty(X, Y, SHIPLENGTH))
+                    {
+                        X = r.Next(1, FormGame.SIZEMAP);
+                        Y = r.Next(1, FormGame.SIZEMAP);
+                    }
+                    for (int k = Y; k < Y + SHIPLENGTH; k++)
+                    {
+                        mapPlayer[X, k] = 1;
+                    }
+
+
+
+                    COUNTSHIPS--;
+                    if (COUNTSHIPS <= 0)
+                        break;
+                }
+                DELTA += 4;
+                SHIPLENGTH--;
+            }
+            return mapPlayer;
+        }
+
+        public bool Shoot()
+        {
+            bool hit = false;
+            Random r = new Random();
+
+            int posX = r.Next(1, FormGame.SIZEMAP);
+            int posY = r.Next(1, FormGame.SIZEMAP);
+
+            while (allOpponentButtons[posX, posY].BackColor == Color.Blue || allOpponentButtons[posX, posY].BackColor == Color.Black)
+            {
+                posX = r.Next(1, FormGame.SIZEMAP);
+                posY = r.Next(1, FormGame.SIZEMAP);
+            }
+
+            if (opponentMap[posX, posY] != 0)
+            {
+                hit = true;
+                opponentMap[posX, posY] = 0;
+                allOpponentButtons[posX, posY].BackColor = Color.Blue;
+                allOpponentButtons[posX, posY].Text = "X";
+            }
+            else
+            {
+                hit = false;
+                allOpponentButtons[posX, posY].BackColor = Color.Black;
+            }
+            if (hit)
+                Shoot();
+            return hit;
+        }
+    }
+}
